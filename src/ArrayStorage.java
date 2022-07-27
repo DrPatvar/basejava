d8b25a8276502a8aa061c9be7503640f1cbd6e5d
DrPatvar <Shurkoff@gmail.com>
2022-07-27 15:23:37 +0300

    Редактироваиние стилей

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,25 +1,21 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[5];
+    Resume[] storage = new Resume[10000];
 
     void clear() {
-        System.out.println("отчистка массива");
-        for (int i = 0; i <storage.length ; i++) {
-            if (storage[i]!=null){
-                storage[i]=null;
+        System.out.println("Отчистка массива");
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
             }
         }
     }
 
     void save(Resume r) {
-        for (int i = 0; i <storage.length ;i++) {
-            if (storage[i]==null) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
                 storage[i] = r;
                 break;
             }
@@ -28,30 +24,32 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        for (Resume resum: storage
-             ) {
-            try {if (resum.uuid.equals(uuid)) return resum;
-            }catch (NullPointerException e){
+        for (Resume resum : storage
+        ) {
+            try {
+                if (resum.uuid.equals(uuid)) return resum;
+            } catch (NullPointerException e) {
             }
-        }return null;
+        }
+        return null;
     }
 
     void delete(String uuid) {
         System.out.println("Удаление элемента из массива");
         Resume[] cloneRes = new Resume[storage.length];
-        for (int i = 0; i <storage.length ; i++) {
-            cloneRes[i]=storage[i];
+        for (int i = 0; i < storage.length; i++) {
+            cloneRes[i] = storage[i];
         }
-        try{
-            for (int i = 0; i <storage.length ; i++) {
-                if (storage[i].uuid.equals(uuid)){
-                    for (int j = i+1; j <cloneRes.length; j++) {
-                        storage[i]=cloneRes[j];
+        try {
+            for (int i = 0; i < storage.length; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    for (int j = i + 1; j < cloneRes.length; j++) {
+                        storage[i] = cloneRes[j];
                     }
                     break;
                 }
             }
-        }catch (NullPointerException e){
+        } catch (NullPointerException e) {
 
         }
 
@@ -65,9 +63,10 @@ public class ArrayStorage {
     }
 
     int size() {
+        System.out.println("Количество элементов в массиве: ");
         int count = 0;
-        for (int i = 0; i <storage.length ; i++) {
-            if (storage[i]!=null)count++;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) count++;
         }
         return count;
     }
