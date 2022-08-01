3ea7688a6e31d9fac1ecb5d529592eabc792e5a4
DrPatvar <Shurkoff@gmail.com>
2022-08-01 10:40:36 +0300

    отредактирован метод Get

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -7,10 +7,10 @@ public class ArrayStorage {
 
     void clear() {
         System.out.println("Обнуление массива: ");
-        for (int i = 0; i < size-1; i++) {
+        for (int i = 0; i < size - 1; i++) {
             storage[i] = null;
         }
-        size=0;
+        size = 0;
     }
 
     void save(Resume r) {
@@ -19,14 +19,10 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        for (Resume resum : storage
-        ) {
-            try {
-                if (resum.uuid.equals(uuid)) return resum;
-            } catch (NullPointerException e) {
-            }
-        }
-        return null;
+        for (int i = 0; i <size ; i++) {
+            storage[i].equals(uuid);
+            return storage[i];
+        }return null;
     }
 
     void delete(String uuid) {
@@ -46,17 +42,15 @@ public class ArrayStorage {
                 }
             }
         } catch (NullPointerException e) {
-
         }
-
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-            Resume[] resumes = new Resume[size];
-        for (int i = 0; i < size ; i++) {
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size; i++) {
             resumes[i] = storage[i];
         }
         return resumes;
