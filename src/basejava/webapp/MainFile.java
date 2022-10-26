4a14f9d1ce9903c6352aae888c72639b664686dc
DrPatvar <Shurkoff@gmail.com>
2022-10-26 15:43:14 +0300

    HW_9 recursion

--- src/basejava/webapp/MainFile.java
+++ src/basejava/webapp/MainFile.java
@@ -1,25 +1,22 @@
 package basejava.webapp;
 
 import java.io.File;
-import java.io.IOException;
 
 public class MainFile {
-    private static int count = 0;
+    static String repeat(int n, String value) {
+        return new String(new char[n]).replace("\0", value);
+    }
 
-    public static   void listFile(File dir) {
-        int count = 0;
-       final String level = new String(new char[count]).replace("\0", "  ");
-        File[] list = dir.listFiles();
-        if (list != null) {
-            for (File file : list
-            ) {
-                if (file.isFile()) {
-                    System.out.println ("File: " + file.getName());
-                } else if (file.isDirectory()) {
-                    System.out.println (level + "Directory: " + file.getName());
-                    listFile(file);
-                    count++;
-                }
+    public static void listFile(File file, int count) {
+        final String space = repeat(count, "    ");
+        File[] files = file.listFiles();
+        for (File f : files
+        ) {
+            if (f.isDirectory()) {
+                System.out.println( space + "Directory: " + f.getName());
+                listFile(f,count +1);
+            } else {
+                System.out.println(space + "File: " + f.getName());
             }
         }
     }
@@ -27,14 +24,7 @@ public class MainFile {
 
     public static void main(String[] args) {
         File path = new File("./src/");
-        String filePath = ".\\.gitignore";
+        listFile(path,0);
 
-        File file = new File(filePath);
-        try {
-            System.out.println(file.getCanonicalPath());
-        } catch (IOException e) {
-            throw new RuntimeException("Error", e);
-        }
-        listFile(path);
     }
 }
