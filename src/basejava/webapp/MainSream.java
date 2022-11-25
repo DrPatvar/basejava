ba9e6bed4603fed8501c321faaaf543d69507c3e
DrPatvar <Shurkoff@gmail.com>
2022-11-25 09:09:24 +0300

    HW_12 stream_2

--- src/basejava/webapp/MainSream.java
+++ src/basejava/webapp/MainSream.java
@@ -5,35 +5,21 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.*;
 
 public class MainSream {
    private int[] array = new int[]{6,4,2,5,1,2,6,3,6};
 
     private int minValue (int[] values){
-       int result = Arrays.stream(values).
-                distinct().
-                sorted().
-                reduce(0, (a, b) -> 10 * a + b);
-        return result;
+       return Arrays.stream(values).
+               distinct().
+               sorted().
+               reduce(0, (a, b) -> 10 * a + b);
+
     }
 
     private List<Integer> oddOrEven(List<Integer> integers){
-        List<Integer> list = new ArrayList<>();
-        int sum = integers.stream().reduce(0,Integer::sum);
-        if (sum % 2 == 1){
-             list =  integers.stream().filter(o -> o % 2 !=0).collect(Collectors.toList());
-            integers.removeAll(list);
-            return integers;
-        }
-        else {
-            list =  integers.stream().filter(o -> o % 2 ==0).collect(Collectors.toList());
-            integers.removeAll(list);
-        }
-        return integers;
+        Integer number = (integers.stream().reduce(0,Integer::sum)) % 2;
+        return integers.stream().filter(x -> x % 2 == number).collect(Collectors.toList());
     }
     public static void main(String[] args) {
         List<Integer> integers = new ArrayList<>();
@@ -42,6 +28,6 @@ public class MainSream {
         MainSream mainSream = new MainSream();
 
         System.out.println("мин число из: " + mainSream.minValue(mainSream.array));
-        System.out.println( mainSream.oddOrEven(integers));
+        System.out.println(mainSream.oddOrEven(integers));
     }
 }
